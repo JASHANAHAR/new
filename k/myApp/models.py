89795da9ba32ec6e3f1aeae1f409961d1b44615a
JASHANAHAR <naharjashan13@gmail.com>
2025-01-16 18:09:34 +0530

    new changes

--- k/myApp/models.py
+++ k/myApp/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 from django_countries.fields import CountryField
-from django_countries.widgets import CountrySelectWidget
 
 class UserData(models.Model):
-    GENDER_CHOICE=[
-          ("male","Male"),
-          ("female","Female"),
+    GENDER_CHOICE = [
+        ("male", "Male"),
+        ("female", "Female"),
     ]
     name = models.CharField(max_length=100)
     user_name = models.CharField(max_length=100)
     email = models.EmailField()
     gender = models.CharField(max_length=10)
-    country = CountryField()  # Make sure this is CountryField
+    country = CountryField()  # Using CountryField
     password = models.CharField(max_length=100)
     password_strength = models.IntegerField(default=0)
     image = models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.name
+
 
 # class home(models.Model):
 #     home_name = models.CharField(max_length=50) 
